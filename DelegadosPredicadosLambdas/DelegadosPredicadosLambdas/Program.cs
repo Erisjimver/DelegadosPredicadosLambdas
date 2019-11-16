ff9d2fe956f790940d96aefb860e75c6d8c82f48
erisjimver <erisjinver@gmail.com>
2019-11-15 23:13:02 -0500

    First Commit

---
+++ DelegadosPredicadosLambdas/DelegadosPredicadosLambdas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DelegadosPredicadosLambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //creacion del objeto delegado apuntando a mensaje bienvenida
+            ObjetoDelegado ElDelegado = new ObjetoDelegado(mensajeBienvenida.SaludoBienvenida);
+
+            //utilizacion del objeto delegado para llamar al metodo saludoBienvenida
+            ElDelegado();
+
+            ElDelegado = new ObjetoDelegado(MensajeDespedida.SaludoDespedida);
+            ElDelegado();
+        }
+        //definicion del objeto delegado
+        delegate void ObjetoDelegado();
+    }
+
+    class mensajeBienvenida
+    {
+        public static void SaludoBienvenida()
+        {
+            Console.WriteLine("Hola acabo de llegar. Que tal");
+        }
+    }
+
+    class MensajeDespedida
+    {
+        public static void SaludoDespedida()
+        {
+            Console.WriteLine("Hola ya me marcho. Hasta luego");
+        }
+    }
+}
